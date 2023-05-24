f91a46b12e3283b475d3145c1153ae84ce912b62
Mary <mashulechka-iva@mail.ru>
2023-05-24 14:47:30 +0300

    Final 21/05

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Set<Notebook> set = new HashSet<>();
-        set.add(new Notebook("Notebook 1", 8, "Windows10", 10000, "HP"));
+        set.add(new Notebook("Notebook 1", 8, "Windows7", 10000, "HP"));
         set.add(new Notebook("Notebook 2", 16, "Windows10", 85000, "Asus"));
         set.add(new Notebook("Notebook 3", 32, "linux", 85000, "Lenovo"));
         set.add(new Notebook("Notebook 4", 64, "linux", 90000, "Lenovo"));
